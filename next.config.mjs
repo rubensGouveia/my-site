5d99a95de4c224807556cbceed260cff165f5869
Rubens <rubensgouveia@ioasys.com.br>
2024-05-15 19:02:24 -0300

    unoptimizing images

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,7 @@ const nextConfig = {
   output: 'export',
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-       
-      },
-    ],
+    unoptimized: true,
   },
 };
 
